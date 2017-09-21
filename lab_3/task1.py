348163058896bb084f727100174f71390cc58f24
MoweMax <mowemax@gmail.com>
2017-09-21 18:06:08 +0300

    3

---
+++ lab_3/task1.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python3.6
+
+a = 222
+b = 123456
+print (a + b)
---
+++ lab_3/task3.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python3.6
+
+g = float(input("Введіть вашу вагу: "))
+h = float(input("Введіть ваш зріст: "))
+print ("BMI = ", g / (h ** h) )
