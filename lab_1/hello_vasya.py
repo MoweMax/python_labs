ca5bb64127bb0a94646e7b3f090da3f393eac048
MoweMax <mowemax@gmail.com>
2017-09-21 17:53:25 +0300

    1

--- lab_1/hello_vasya.py
+++ lab_1/hello_vasya.py
@@ -1,17 +1,3 @@
 #! /usr/bin/env python3.6
 
-import dis
-
-import sys
-sys.version
-print(sys.version)
-print()
-
-a = 2
-b = 4
-a+b
-
-def add(a, b):
-    return a+b
-
-dis.dis(add)
+print("Привіт, Максим c:")
